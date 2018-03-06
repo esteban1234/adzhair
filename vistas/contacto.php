ef8567b8af42ee8888a184d1c5d830b2dc0f41fc
esteban1234 <tva_libra@hotmail.com>
2018-03-06 17:31:19 -0600

    nosotros listo

--- vistas/contacto.php
+++ vistas/contacto.php
@@ -3,97 +3,13 @@
 <body>
 	
 
-  <header>
-    <figure class="logo pull-left">
-        <img src="../img/dafo.png" alt="DAFO" class="img-responsive">
-    </figure>
-
-    <div class="op-menu">
-      <i class="fa fa-bars fa-1x i-menu" aria-hidden="true"></i>
-    </div>
-
-    <nav class="pull-right">
-        <ul>
-            <a class="select" href="../index.php">INICIO</a>
-            <a class="select" href="nosotros.php">NOSOTROS</a>
-            <a class="select" href="servicios.php">SERVICIOS</a>
-            <a class="select" id="selector" href="contacto.php">CONTACTO</a>
-        </ul>
-    </nav>
-</header>
-
-<div class="banner4">
-   <div class="fondo4">
-        <p class="uno">EXPERTOS EN <br>  DERECHO CIVIL Y DERECHO LABORAL</p>
-   </div>
-</div>
-
-<p class="textcontac"> Si desea más información sobre nuestros servicios, puede contactarnos a través de nuestros teléfonos, correo electrónico o en nuestras oficinas. </p>
-
-<section id="contact" class="content-section text-center">
-        <div class="contact-section">
-            <div class="container">
-              <div class="row">
-                <div class="col-md-8 col-md-offset-2">
-                  <form class="form-horizontal" onSubmit="return false">
-                    <div class="form-group">
-                      <label for="exampleInputName2">Nombre Completo</label>
-                      <input type="text"  class="form-control" id="txtNOMBRE" placeholder="">
-                    </div>
-                    <div class="form-group">
-                      <label for="exampleInputName2">Teléfono</label>
-                      <input type="text"  class="form-control" id="txtTELEFONO" placeholder="">
-                    </div>
-                    <div class="form-group">
-                      <label for="exampleInputEmail2">Correo</label>
-                      <input type="email"  class="form-control" id="txtCORREO" placeholder="">
-                    </div>
-                    <div class="form-group ">
-                      <label for="exampleInputText">Comentario</label>
-                     <textarea   id="txtCOMENTARIO" class="form-control" placeholder=""></textarea> 
-                    </div>
-                    <div id="_AJAX_PRE_"></div><br>
-                    <button type="submit" class="btn btn-default" onclick="sendCORREO()">Enviar Mensaje</button>
-                  </form>
-
-                  <hr>
-                    <h3 class="do">DATOS DE OFICINA</h3>
-                </div>
-              </div>
-            </div>
-        </div>
-              <br><br>
-         <div class="container">
-        <div class="row">
-          <div class="col-sm-6 col-md-6 col-lg-3 cont-info">
-            <i class="fas fa-map-marker-alt fa-7x"></i>
-            <h2 class="text-center tit-dtch">DIRECCION</h2>
-            <p class="text-center">PENDIENTE</p>
-          </div>
-          <div class="col-sm-6 col-md-6 col-lg-3 cont-info">
-            <i class="fas fa-phone fa-7x"></i>
-            <h2 class="text-center tit-dtch">TELEFONO</h2>
-            <p class="text-center">PENDIENTE</p>
-          </div>
-          <div class="col-sm-6 col-md-6 col-lg-3 cont-info">
-            <i class="far fa-envelope fa-7x"></i>
-            <h2 class="text-center tit-dtch">CORREO</h2>
-            <p class="text-center">info@administradoradafo.com</p>
-          </div>
-          <div class="col-sm-6 col-md-6 col-lg-3 cont-info">
-            <i class="far fa-clock fa-7x"></i>
-            <h2 class="text-center tit-dtch">HORARIO</h2>
-            <p class="text-center">Lunes a Viernes de 9 am a 6 pm</p>
-          </div>
-        </div>
-      </div>
-
-      </section>
-
+  
+<a class="go-top" href="#">Subir</a>
 <?php include 'pie.php' ?>
 <script src="../js/jquery.js"></script>
 <script src="../js/sendCORREO.js"></script>
 <script src="../js/bootstrap.js"></script>
+<script src="../js/arriba.js"></script>
 <script src="../js/funciondthc.js"></script>
 <script src="../js/menu.js"></script>
 <!-- <script src="../js/sendCORREO.js"></script> -->
--- vistas/nosotros.php
+++ vistas/nosotros.php
@@ -2,27 +2,81 @@
 <?php include 'encabezado2.php' ?>
 <body>
 
-<article class="encabezado">
-  <article class="encabezadouno">
-    <div class="dentro">
-      <li>Contsrución</li>
-      <li>Arquitectura</li>
-      <li>Diseño</li>
-      <li>Remodelación</li>
-      <li>Planos</li>
-      <li>Ingenieria</li>
-    </div>
-  </article>
-
+<header>
+  <article class="encabezado">
+    <article class="encabezadouno">
+      <div class="dentro">
+        <li>Contsrución</li>
+        <li>Arquitectura</li>
+        <li>Diseño</li>
+        <li>Remodelación</li>
+        <li>Planos</li>
+        <li>Ingenieria</li>
+      </div>
+    </article>
   
-</article>
+    
+  </article>
+  
+  <figure class="logo2">
+    <img src="../img/adzhair.png" alt="ADZHAIR" class="img-responsive">
+  </figure>
+  
+  <div class="menu">
+    <ul>
+      <a class="selector" href="../index.php">INICIO</a>
+      <a class="selector" id="select" href="nosotros.php">NOSOTROS</a>
+      <a class="selector" href="servicios.php">SERVICIOS</a>
+      <a class="selector" href="contacto.php">CONTACTO</a>
+    </ul>
+  </div>
+
+  <div class="banner">
+    <div class="fondo">
+      <p>CONSTRUYENDO TUS SUEÑOS</p>
+    </div>
+  </div>
+</header>
+
+ <div class="section" style="padding: 5% 0%;">
+
+  <h1 class="text-center" style="color: #8B124F;">¿QUIENES SOMOS?</h1> <br><br>
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <img src="../img/constructora.jpg"
+            class="img-responsive">
+          </div>
+          <div class="col-md-6">
+            <p class="texto">Somos una empresa al servicio de nuestros clientes con la finalidad de ofrecer la mejor arquitectura y edificación de nuestros inmuebles. Somos una empresa llena de éxitos y clientes satisfechos, nuestras imágenes hablan por sí mismas tanto en proyectos, obras en proceso y obras terminadas. En cada departamento tenemos personal capacitado para que su construcción culmine como siempre se la imagino, conozca más de esta empresa a través de nuestros asesores. Somos una empresa líder en diseño y edificación de vivienda residencial, inmuebles comerciales e industriales, ofrecemos la mejor arquitectura en sus diferentes estilos y los traducimos en edificación de la más alta calidad. Entregas en tiempos contratados y presupuestos responsables hacen que tengamos el mayor número de clientes satisfechos, construimos todas las obras, acérquese a nuestros asesores y sea parte de esta gran empresa.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-6">
+            <p class="texto2">ADZHAIR es una empresa dedicada al servicio inmobiliario, a nivel residencial, comercial, industrial y turístico, así como terrenos urbanos y rústicos. Nuestro esfuerzo es seguir prevaleciendo en el mercado con el verdadero sentido del promotor inmobiliario, ofreciendo nuestra capacidad jurídica, financiera, arquitectónica y de construcción que hacen de nuestra empresa la mejor para que usted pueda vender, comprar, rentar o traspasar el inmueble deseado, y con esto poder obtener el mejor servicio y satisfacción, mismos que hacen de esta empresa la mejor en su ramo a nivel residencial.</p>
+          </div>
+          <div class="col-md-6">
+            <img src="../img/modelada.jpg"
+            class="img-responsive">
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="boton">
+      <a href="contacto.php"><button type="button" class="btn btn-danger btn-lg">CONTACTANOS</button></a>
+    </div>
 
 <a class="go-top" href="#">Subir</a>
 <?php include 'pie.php' ?>
 
 
 <script src="../js/jquery.js"></script>
-<script src="js/arriba.js"></script>
+<script src="../js/arriba.js"></script>
 <script src="../js/bootstrap.js"></script>
 <script src="../js/menu.js"></script>
 </body>
