d4c3dc4d2c1c7873a6a66350c16e96d2c44e8e0d
esteban1234 <tva_libra@hotmail.com>
2018-03-06 18:02:17 -0600

    servicios a la mitad

--- vistas/servicios.php
+++ vistas/servicios.php
@@ -2,6 +2,154 @@
 <?php include 'encabezado2.php' ?>
 <body>
 	
+	<header>
+  <article class="encabezado">
+    <article class="encabezadouno">
+      <div class="dentro">
+        <li>Contsrución</li>
+        <li>Arquitectura</li>
+        <li>Diseño</li>
+        <li>Remodelación</li>
+        <li>Planos</li>
+        <li>Ingenieria</li>
+      </div>
+    </article>
+  
+    
+  </article>
+  
+  <figure class="logo2">
+    <img src="../img/adzhair.png" alt="ADZHAIR" class="img-responsive">
+  </figure>
+  
+  <div class="menu">
+    <ul>
+      <a class="selector" href="../index.php">INICIO</a>
+      <a class="selector" href="nosotros.php">NOSOTROS</a>
+      <a class="selector" id="select" href="servicios.php">SERVICIOS</a>
+      <a class="selector" href="contacto.php">CONTACTO</a>
+    </ul>
+  </div>
+
+  <div class="banner2">
+    <div class="fondo">
+      <p>SERVICIOS A TU MEDIDA</p>
+    </div>
+  </div>
+</header>
+
+<h1 class="tits">SERVCIOS DE DISEÑO</h1>
+<hr>
+
+<div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png"
+            class="img-responsive">
+            <h2 class="text-center">Estructural</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p>
+          </div>
+          <div class="col-md-4">
+            <img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png"
+            class="img-responsive">
+            <h2 class="text-center">Interiores</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p>
+          </div>
+          <div class="col-md-4">
+            <img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png"
+            class="img-responsive">
+            <h2 class="text-center">Planos</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p>
+          </div>
+        </div> <br><br>
+
+        <div class="row">
+          <div class="col-md-4 col-md-offset-2">
+            <img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png"
+            class="img-responsive">
+            <h2 class="text-center">Arquitectonico</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p>
+          </div>
+          <div class="col-md-4">
+            <img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png"
+            class="img-responsive">
+            <h2 class="text-center">Render</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p>
+          </div>
+
+        </div>
+      </div>
+    </div> <br><br>
+
+    <h1 class="tits">SERVCIOS DE CONSTRUCCIÓN</h1>
+<hr>
+
+<div class="section">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png"
+            class="img-responsive">
+            <h2 class="text-center">Supervisión</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p>
+          </div>
+          <div class="col-md-4">
+            <img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png"
+            class="img-responsive">
+            <h2 class="text-center">Asesoría</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p>
+          </div>
+          <div class="col-md-4">
+            <img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png"
+            class="img-responsive">
+            <h2 class="text-center">Habitacional</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p>
+          </div>
+        </div> <br><br>
+
+        <div class="row">
+          <div class="col-md-4 col-md-offset-2">
+            <img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png"
+            class="img-responsive">
+            <h2 class="text-center">Comercial</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p>
+          </div>
+          <div class="col-md-4">
+            <img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png"
+            class="img-responsive">
+            <h2 class="text-center">Remodelación</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p>
+          </div>
+        </div>
+
+        <div class="boton">
+      <a href="contacto.php"><button type="button" class="btn btn-danger btn-lg">CONTACTANOS</button></a>
+    </div>
+
+      </div>
+    </div>
+
 	
 
 <a class="go-top" href="#">Subir</a>
